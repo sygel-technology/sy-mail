dbb422a613efa13c5913cec1b80cb12a8d9d16bd
Angel Rivas <angel.rivas@sygel.es>
2026-08-28 12:00:14 +0200

    [T-10109][ADD] sy_mail_activity_schedule_description_layout

---
+++ sy_mail_activity_schedule_description_layout/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2026 Ángel Rivas <angel.rivas@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Mail Activity Schedule Description Layout",
+    "summary": "Increases the note field size when scheduling activities",
+    "version": "18.0.1.0.0",
+    "category": "Productivity/Discuss",
+    "website": "https://github.com/sygel-technology/sy-mail",
+    "author": "Sygel",
+    "license": "AGPL-3",
+    "depends": [
+        "mail",
+    ],
+    "data": [
+        "views/mail_activity_schedule_views.xml",
+    ],
+    "installable": True,
+}
---
+++ sy_mail_activity_schedule_description_layout/readme/DESCRIPTION.md
@@ -0,0 +1,1 @@
+Increases the note field size when scheduling activities
